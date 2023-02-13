3ddc6ef1ceab1417c5a9666ae87c7141436cb62f
Senator09 <jaiteh@purdue.edu>
2023-02-13 12:53:44 -0500

    This is the skeleton file with no functionality

---
+++ PetesParkingMgt/src/test/java/com/student/PetesParkingMgt/PetesParkingMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.PetesParkingMgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetesParkingMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
