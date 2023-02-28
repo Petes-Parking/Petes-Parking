0f71e0c77adb955a4af6e23992546a9f5a31a82f
naiaechevarria <nechevar@purdue.edu>
2023-02-27 22:03:22 -0500

    Add profile page to web app. Add "save changes button"

---
+++ src/main/java/com/petesparkingmgt/ctl/ProfileCtl.java
@@ -0,0 +1,15 @@
+package com.petesparkingmgt.ctl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileCtl {
+
+    @GetMapping("/profile")
+    public String ProfilePage() {
+        return "profilePage";
+    }
+
+
+}
