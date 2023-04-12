044d319fbe0229a0524154714add5356bc5129a1
maxfuligni <maxfuligni@gmail.com>
2023-04-12 15:36:11 -0400

    fixed unit tests from fae7481

--- src/test/java/com/petesparkingmgt/AllTests.java
+++ src/test/java/com/petesparkingmgt/AllTests.java
@@ -1,21 +1,31 @@
 package com.petesparkingmgt;
 
-import com.petesparkingmgt.UnitControllerLayerTest;
 
 import com.petesparkingmgt.UserRepositoryLayerTests;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.junit.platform.commons.annotation.Testable;
 
-//@RunWith(JUnitPlatform.class)
-@DisplayName("All Tests")
-@SuiteClasses({
+@RunWith(JUnitPlatform.class)
+@SuiteDisplayName("All Tests")
+
+@SelectClasses({
+        AdminControllerTest.class,
+        CarpoolCtlTest.class,
+        CarpoolUsersCtlTest.class,
+        HistoryCtlTest.class,
+        MainCtlTest.class,
+        ParkingPalsCtlTest.class,
+        ProfileCtlTest.class,
         UserRepositoryLayerTests.class,
-        UnitControllerLayerTest.class,
+        UserControllerTest.class,
         LeaderboardControllerUnitTest.class,
         ManagePointsControllerTest.class
 
